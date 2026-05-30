9092f0f9b0eb8c195f9e410e46b449db0e14f1e3
Traianos <traianos1985@gmail.com>
2026-05-31 00:16:40 +0300

    Run due schedules during autonomy cycle

--- executor/autonomy_v3.py
+++ executor/autonomy_v3.py
@@ -2,7 +2,7 @@ import time
 
 from executor.battery_guard import battery_guard
 from executor.daily_brief import daily_brief
-from executor.scheduler_agent import list_schedules
+from executor.scheduler_agent import list_schedules, run_due_schedules
 from executor.agent_review import review_last
 
 def autonomy_cycle():
@@ -11,6 +11,7 @@ def autonomy_cycle():
         "battery": battery_guard(),
         "daily_brief": daily_brief(),
         "scheduled": list_schedules(),
+        "scheduled_executed": run_due_schedules(),
         "review": review_last()
     }
 
